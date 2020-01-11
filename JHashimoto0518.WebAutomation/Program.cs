d4b7e4a961f5439efec350a6ca5b13bc99655c22
橋本淳一 <junichi_hashimoto@hotmail.co.jp>
2020-01-11 11:57:57 +0900

    Make Project

---
+++ JHashimoto0518.WebAutomation/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JHashimoto0518.WebAutomation
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
